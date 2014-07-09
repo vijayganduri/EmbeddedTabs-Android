be4b56931fb2467845785c77b516714fc4411c89
Vijay Ganduri <vijayganduri@gmail.com>
2014-07-09 14:25:19 +0530

    initial commit

---
+++ gen/com/vijayganduri/embeddedtabs/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.vijayganduri.embeddedtabs;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
---
+++ src/com/vijayganduri/dietplancontrol/MainActivity.java
@@ -0,0 +1,15 @@
+package com.vijayganduri.dietplancontrol;
+
+import android.os.Bundle;
+import com.actionbarsherlock.app.SherlockFragmentActivity;
+import com.vijayganduri.embeddedtabs.R;
+
+public class MainActivity extends SherlockFragmentActivity{
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+	}
+	
+}
